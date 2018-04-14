2f0ce1691fc73fc83f92480ef296a84d5281e12a
qloog <quanlongwang@gmail.com>
2018-04-14 17:12:12 +0800

    完善readme

--- application/controllers/Error.php
+++ application/controllers/Error.php
@@ -5,7 +5,7 @@
  * @see http://www.php.net/manual/en/yaf-dispatcher.catchexception.php
  * @author qloog
  */
-class ErrorController extends Yaf\Controller_Abstract {
+class ErrorController extends Yaf_Controller_Abstract {
 
 	//从2.1开始, errorAction支持直接通过参数获取异常
 	public function errorAction($exception) {
--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -5,11 +5,11 @@
  * @desc 默认控制器
  * @see http://www.php.net/manual/en/class.yaf-controller-abstract.php
  */
-class IndexController extends Yaf\Controller_Abstract {
+class IndexController extends Yaf_Controller_Abstract {
 
 	/** 
      * 默认动作
-     * Yaf支持直接把Yaf\Request_Abstract::getParam()得到的同名参数作为Action的形参
+     * Yaf支持直接把Yaf_Request_Abstract::getParam()得到的同名参数作为Action的形参
      * 对于如下的例子, 当访问http://yourhost/demo/index/index/index/name/qloog 的时候, 你就会发现不同
      */
 	public function indexAction($name = "PHPer") {
